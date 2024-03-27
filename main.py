580c90b80cb69936c53561635615aa2ad1d748f1
z4heed <szasaa10@gmail.com>
2024-03-27 14:58:31 +0530

    input

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import numpy as np 
+import numpy as np
+
 
 def matrix():
     """Creates a Matrix with using Numpy Zeroes Function"""
     while True:
         try:
-            rows=int(input("Number of rows :"))
-            columns=int(input("Number of Columns: "))
+            rows, columns = map(int, input("Enter number of rows and columns (with space): ").split())
             A = np.zeros((rows, columns))
             print(A)
             if input("Is this the specified Matrix  --> Y/N ?").upper() == 'Y':
                 break
         except ValueError:
             print("Please enter the new Rows & Columns")
-            
+
+
 matrix()
